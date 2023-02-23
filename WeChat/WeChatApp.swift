3649a64c79cd107829c877035440f828d47e6e7c
莫名 <36976543+fangzan@users.noreply.github.com>
2023-02-23 10:52:33 +0800

    Initial Commit

---
+++ WeChat/WeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeChatApp.swift
+//  WeChat
+//
+//  Created by AC on 2023/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
