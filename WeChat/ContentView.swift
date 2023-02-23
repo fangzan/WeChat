475c611e0bf7286d955c71bf4cef35f8cfa5dc68
莫名 <36976543+fangzan@users.noreply.github.com>
2023-02-23 11:43:55 +0800

    添加TextView a

--- WeChat/ContentView.swift
+++ WeChat/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hello, world!")
             Text("Hello, world!")
+            Text("Hello, world!")
         }
         .padding()
     }
