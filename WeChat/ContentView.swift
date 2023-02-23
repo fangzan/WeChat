fe000ca5817dc4173ceb6fcaa8d7e2f4982f5136
莫名 <36976543+fangzan@users.noreply.github.com>
2023-02-23 11:44:30 +0800

    调整通了

--- WeChat/ContentView.swift
+++ WeChat/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hello, world!")
             Text("Hello, world!")
+            Text("Hello, world!")
         }
         .padding()
     }
